95b8eb0560d141251b2674bcf5f40e0b7ed152fa
Kol_the_guy <28795499+Koltheguy@users.noreply.github.com>
2023-09-12 16:43:49 -0700

    merged development into current

--- README.md
+++ README.md
@@ -3,6 +3,3 @@ I'm using this space to learn and experiment.
 
 -Tom
 
-
-
-./helloWorld>  python manage.py runserver
--- helloWorld/coffee/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Error 418 I'm a teapot <a href='https://www.rfc-editor.org/rfc/rfc2324'>RFC2324</a>")
