8cd4fff221d55becb5c93c308e3ab95998e9287a
mustachemo <mhasan1080@gmail.com>
2023-09-16 23:18:58 -0700

    running into assets is undefined problem

--- data_clean/__init__.py
+++ data_clean/__init__.py
@@ -4,7 +4,7 @@ from flask_assets import Environment, Bundle
 app = Flask(__name__)
 assets = Environment(app)
 
-scss_bundle = Bundle('static/*.scss', filters='pyscss', output='static/styles.css')
+scss_bundle = Bundle('static/main.scss', filters='pyscss', output='static/styles.css', depends='static/*.scss')
 assets.register('scss_all', scss_bundle)
 
 from data_clean import routes
