65ada0bc93bea61f6e89e227f919c5797efdb8e5
mustachemo <mhasan1080@gmail.com>
2023-09-16 23:24:16 -0700

    comiplation from scss to css works

--- data_clean/__init__.py
+++ data_clean/__init__.py
@@ -4,7 +4,7 @@ from flask_assets import Environment, Bundle
 app = Flask(__name__)
 assets = Environment(app)
 
-scss_bundle = Bundle('static/main.scss', filters='pyscss', output='static/styles.css', depends='static/*.scss')
+scss_bundle = Bundle('styles/main.scss', filters='pyscss', output='styles/styles.css', depends='styles/*.scss')
 assets.register('scss_all', scss_bundle)
 
 from data_clean import routes
