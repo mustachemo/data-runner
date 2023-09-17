1190ba1012a2c865886c09b366e5bfc55c9c1a9a
mustachemo <mhasan1080@gmail.com>
2023-09-16 23:13:51 -0700

    attempting to add scss static files

--- data_clean/__init__.py
+++ data_clean/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from flask_assets import Environment, Bundle
 
 app = Flask(__name__)
+assets = Environment(app)
+
+scss_bundle = Bundle('static/*.scss', filters='pyscss', output='static/styles.css')
+assets.register('scss_all', scss_bundle)
 
 from data_clean import routes
