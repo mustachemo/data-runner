1bc19be42cb5702a028fe98e494a9bde91846b2f
mustachemo <mhasan1080@gmail.com>
2023-09-16 17:46:22 -0700

    added boilerplate code to check route/templates

--- data_clean/routes.py
+++ data_clean/routes.py
@@ -1,11 +1,16 @@
 from data_clean import app
+from flask import render_template, request, redirect, url_for
 
 
 @app.route('/')
 def interface():
-    return "<p>Hello, World!</p>"
+    return render_template('base.html')
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return "<p>Sorry, this page was not found.</p>", 404
+    return "<p>Sorry, this page does not exist.</p>", 404
+
+@app.errorhandler(500)
+def page_not_found(error):
+    return "<p>Sorry, something went wrong.</p>", 500
