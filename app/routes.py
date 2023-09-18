fce896efa5af786fa75f7ac31312fbdbd5940cd8
mustachemo <mhasan1080@gmail.com>
2023-09-18 14:53:45 -0700

    reoganized files

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,20 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from app.middleware.allowed_file import allowed_file
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def interface():
-    flash('This is a flash message', 'info')
-    flash('This is another flash message', 'warning')
-    flash('This is a third flash message', 'danger')
-    flash('This is a fourth flash message', 'success')
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part in request object', 'warning')
+            return render_template('home.html')
+        
+
+    # flash('This is a flash message', 'info')
+    # flash('This is another flash message', 'warning')
+    # flash('This is a third flash message', 'danger')
+    # flash('This is a fourth flash message', 'success')
     return render_template('home.html')
 
 
