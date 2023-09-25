d6e3b0074daba693f1dc00a2f890811a06619335
mustachemo <mhasan1080@gmail.com>
2023-09-25 14:23:06 -0700

    dash works

--- app/middleware/pandas_cleaning.py
+++ app/middleware/pandas_cleaning.py
@@ -1,4 +1,37 @@
 import pandas as pd
+import os
+from dash import Dash, html
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+parent_directory = os.path.dirname(current_directory)
+relative_path = "static/dirty-data"
+target_directory_path = os.path.join(parent_directory, relative_path)
+
+df = pd.read_csv(os.path.join(target_directory_path, 'food_coded.csv'))
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
+
+
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H4(children='US Agriculture Exports (2011)'),
+    generate_table(df)
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 def checkFileContents(file):
     if file.filename.endswith('.csv'):
