fe3041b926891590fd94a2cd0aa8a0cabd1062c1
Kol_the_guy <28795499+Koltheguy@users.noreply.github.com>
2023-09-27 07:47:16 -0700

    fixes #17 resolved callback bug added csv as default value (note "_" generally denotes an unused parameter)

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -50,21 +50,23 @@ app.layout = html.Div([
         style_cell={'textAlign': 'left'}, # left align text in columns for readability
         # fixed_rows={'headers':True, 'data':1}  # Fix header rows at the top
     ),
-    dcc.RadioItems(['csv', 'xsls','pdf', 'html', 'xml'],  id='radio-items'), 
-    dcc.Download(id="download-text")
+    dcc.RadioItems(['csv', 'xsls','pdf', 'html', 'xml'],  id='radio-items', value='csv'), 
+    html.Button("Download", id="btn-download"),
+    dcc.Download(id="download-file"),
 ])
 
 @callback(
-    Output("download-text", "data"),
-    Input('radio-items', 'value'),
+    Output("download-file", "data"),
+    State('radio-items', 'value'),
+    Input("btn-download", "n_clicks"),
     prevent_initial_call=True,
 )
-def download_specific_file(value):
-    if value == 'csv':
+def download_specific_file(fileType, _):
+    if fileType == 'csv':
         return dict(content=df.to_csv(index=False), filename="data.csv")
-    if value == 'xml':
+    if fileType == 'xml':
         return dict(content=df.to_xml(index=False), filename="data.xml")
-    if value == 'html':
+    if fileType == 'html':
         return dict(content=df.to_html(index=False), filename="data.html")
 
 
