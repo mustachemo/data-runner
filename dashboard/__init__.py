0d8a4bb73faead09190c5d2dc9dd67fac71522d1
mustachemo <mhasan1080@gmail.com>
2023-10-05 16:14:00 -0700

    loading animation done, also [check desc]
    removed data handler and dcc.Store() as they slowed us down and created unndeeded complexity

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -2,11 +2,12 @@ from dash import Dash, Input, Output, State, callback, callback_context, excepti
 import dash_bootstrap_components as dbc
 import pandas as pd
 
-from dashboard.utils.datahandler import DataHandler
+from dashboard.utils.datacleaner import DataCleaner as dc
 from .layout import layout
 
 # This is the data handler object (see dashboard/utils/datahandler.py)
-data_handler = DataHandler(pd.DataFrame())
+dc = dc()
+df = pd.DataFrame()
 # This is the main app object
 app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 # Improves load time by not loading all callbacks at once. 5-10% improvement
@@ -15,6 +16,27 @@ app.config.suppress_callback_exceptions = True
 app.layout = layout
 
 
+###################### UPLOAD FILE ######################
+@callback(
+    Output('editable-table', 'data'),
+    Output('editable-table', 'columns'),
+    Input('upload-data', 'contents'),
+    State('upload-data', 'filename'),
+    State('upload-data', 'last_modified')
+)
+def upload_file(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is None:
+        raise exceptions.PreventUpdate
+
+    df.df = dc.parse_contents(
+        list_of_contents[0], list_of_names[0], list_of_dates[0])
+    columns = [{'name': col, 'id': col, "selectable": True, "renamable": True,
+                "clearable": True, "hideable": True, "deletable": True} for col in df.df.columns]
+
+    return df.df.to_dict('records'), columns
+
+
+###################### DOWNLOAD FILE ######################
 @callback(  # This is a callback that will download the file
     Output("download-file", "data"),
     [Input("btn-download", "n_clicks")],
@@ -23,36 +45,38 @@ app.layout = layout
      State('editable-table', 'columns')],
     prevent_initial_call=True,
 )
-def download_specific_files(_, fileType, dataTableData, current_columns):
-    return data_handler.download_specific_files(_, fileType, dataTableData, current_columns)
+def download_specific_files(self, _, fileType, dataTableData, current_columns):
+    self.df = pd.DataFrame.from_dict(data=dataTableData)
+
+    # Renaming columns based on current columns in DataTable
+    renaming_dict = {col['id']: col['name'] for col in current_columns}
+    self.df.rename(columns=renaming_dict, inplace=True)
+
+    if fileType == 'csv':
+        return dict(content=self.df.to_csv(index=False), filename="data.csv")
+    if fileType == 'xml':
+        return dict(content=self.df.to_xml(index=False), filename="data.xml")
+    if fileType == 'html':
+        return dict(content=self.df.to_html(index=False), filename="data.html")
 
 
-@app.callback(  # This is a callback that will highlight the selected columns
+###################### HIGHLIGHT COLUMNS ######################
+@callback(  # This is a callback that will highlight the selected columns
     Output('editable-table', 'style_data_conditional'),
     Input('editable-table', 'selected_columns')
     # Input('editable-table', 'selected_rows')
 )
 def highlight_column(selected_columns):
-    return data_handler.highlight_column(selected_columns)
+    styles = []
 
+    if selected_columns:
+        styles.extend([{'if': {'column_id': col}, 'background_color': '#D2F3FF'}
+                       for col in selected_columns])
 
-@callback(  # This is a callback that will upload the file and cache it
-    Output('df-store', 'data'),
-    Input('upload-data', 'contents'),
-    State('upload-data', 'filename'),
-    State('upload-data', 'last_modified')
-)
-def upload_file_and_cache(list_of_contents, list_of_names, list_of_dates):
-    return data_handler.upload_file_and_cache(list_of_contents, list_of_names, list_of_dates)
+    # if selected_rows:
+    #     styles.extend([{'if': {'row_index': row}, 'background_color': '#7FFF7F'} for row in selected_rows])
 
-
-@callback(  # This is a callback that will update the table with the data from the cache
-    Output('editable-table', 'data'),
-    Output('editable-table', 'columns'),
-    Input('df-store', 'data')
-)
-def update_table(data):
-    return data_handler.update_table(data)
+    return styles
 
 
 if __name__ == '__main__':
--- dashboard/layout.py
+++ dashboard/layout.py
@@ -6,15 +6,12 @@ data = [["csv", "csv"], ["xsls", "xsls"], [
     "pdf", "pdf"], ["html", "html"], ["xml", "xml"]]
 
 layout = html.Div([  # This is the main layout of the app
-    dcc.Store(id='df-store'),  # This is the store that will cache the data
-    dcc.Download(id="download-file"),
-
     html.Div([
         dcc.Upload(  # This is the upload button
             id='upload-data',
             children=dmc.Button("Upload File",
                                 style={"backgroundColor": "#0C7FDA"}),
-            multiple=True  # Allow multiple files to be uploaded
+            multiple=True
         ),
         html.Div([  # This is the dropdown and download button
             dmc.Select(
@@ -29,7 +26,9 @@ layout = html.Div([  # This is the main layout of the app
             ),
             dmc.Button("Export",
                        id="btn-download",
-                       style={"backgroundColor": "#0C7FDA"}),
+                       style={"backgroundColor": "#0C7FDA"}
+                       ),
+            dcc.Download(id="download-file")
         ], style={"display": "flex", "justifyContent": "flex-end", "alignItems": "center", "gap": "1rem"}
         ),
     ], style={'margin': '1rem auto 0 auto', 'width': '90%', "display": "flex", "justifyContent": "space-between", "alignItems": "center"}
@@ -37,26 +36,24 @@ layout = html.Div([  # This is the main layout of the app
 
     dcc.Loading(
         id="loading-table",
-        type="default",  # You can use other types like "default", "cube", "dot", or "circle"
-        fullscreen=False,  # True, False, or "auto"
-        children=[
-            dash_table.DataTable(  # This is the table that will display the data
-                id='editable-table',  # Assign an ID to the DataTable component
-                editable=True,  # Enable editing,
-                column_selectable="multi",  # This enables column selection
-                row_selectable='multi',  # This enables row selection
-                # This enables virtualization, which allows large data sets to be rendered efficiently
-                virtualization=True,
-                # selected_columns=[],
-                # selected_rowss=[],
-                sort_action='native',  # This enables data to be sorted by the user
-                filter_action='native',  # This enables data to be filtered by the user
-                row_deletable=True,  # This enables users to delete rows
-                style_table={'height': '70vh', 'width': '90%',
-                             'overflowX': 'auto', 'margin': '1rem auto 0 auto'},
-                # left align text in columns for readability
-                style_cell={'textAlign': 'left'},
-                # fixed_rows={'headers':True, 'data':1}  # Fix header rows at the top
-            )
-        ])
+        type="cube",
+        fullscreen=False,
+        children=dash_table.DataTable(  # This is the table that will display the data
+            id='editable-table',  # Assign an ID to the DataTable component
+            editable=True,  # Enable editing,
+            column_selectable="multi",  # This enables column selection
+            row_selectable='multi',  # This enables row selection
+            # This enables virtualization, which allows large data sets to be rendered efficiently
+            virtualization=True,
+            # selected_columns=[],
+            # selected_rowss=[],
+            sort_action='native',  # This enables data to be sorted by the user
+            filter_action='native',  # This enables data to be filtered by the user
+            row_deletable=True,  # This enables users to delete rows
+            style_table={'height': '70vh', 'width': '90%',
+                         'overflowX': 'auto', 'margin': '1rem auto 0 auto'},
+            style_cell={'textAlign': 'left'},
+            # fixed_rows={'headers':True, 'data':1}  # Fix header rows at the top
+        )
+    )
 ])
