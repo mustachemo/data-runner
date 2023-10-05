b0ee18e4955dbd08563b678c1a53bd501d4c4d2b
mustachemo <mhasan1080@gmail.com>
2023-10-05 15:56:37 -0700

    attempting to make loading icon

--- dashboard/layout.py
+++ dashboard/layout.py
@@ -35,23 +35,28 @@ layout = html.Div([  # This is the main layout of the app
     ], style={'margin': '1rem auto 0 auto', 'width': '90%', "display": "flex", "justifyContent": "space-between", "alignItems": "center"}
     ),
 
-
-    dash_table.DataTable(  # This is the table that will display the data
-        id='editable-table',  # Assign an ID to the DataTable component
-        editable=True,  # Enable editing,
-        column_selectable="multi",  # This enables column selection
-        row_selectable='multi',  # This enables row selection
-        # This enables virtualization, which allows large data sets to be rendered efficiently
-        virtualization=True,
-        # selected_columns=[],
-        # selected_rowss=[],
-        sort_action='native',  # This enables data to be sorted by the user
-        filter_action='native',  # This enables data to be filtered by the user
-        row_deletable=True,  # This enables users to delete rows
-        style_table={'height': '70vh', 'width': '90%',
-                     'overflowX': 'auto', 'margin': '1rem auto 0 auto'},
-        # left align text in columns for readability
-        style_cell={'textAlign': 'left'},
-        # fixed_rows={'headers':True, 'data':1}  # Fix header rows at the top
-    ),
+    dcc.Loading(
+        id="loading-table",
+        type="default",  # You can use other types like "default", "cube", "dot", or "circle"
+        fullscreen=False,  # True, False, or "auto"
+        children=[
+            dash_table.DataTable(  # This is the table that will display the data
+                id='editable-table',  # Assign an ID to the DataTable component
+                editable=True,  # Enable editing,
+                column_selectable="multi",  # This enables column selection
+                row_selectable='multi',  # This enables row selection
+                # This enables virtualization, which allows large data sets to be rendered efficiently
+                virtualization=True,
+                # selected_columns=[],
+                # selected_rowss=[],
+                sort_action='native',  # This enables data to be sorted by the user
+                filter_action='native',  # This enables data to be filtered by the user
+                row_deletable=True,  # This enables users to delete rows
+                style_table={'height': '70vh', 'width': '90%',
+                             'overflowX': 'auto', 'margin': '1rem auto 0 auto'},
+                # left align text in columns for readability
+                style_cell={'textAlign': 'left'},
+                # fixed_rows={'headers':True, 'data':1}  # Fix header rows at the top
+            )
+        ])
 ])
