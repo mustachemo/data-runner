0602fedc4655b8690639cdae63163d1719bc5549
famin63 <87962678+famin63@users.noreply.github.com>
2023-10-09 20:01:53 -0700

    Removed extra Clean Data button

--- dashboard/layout.py
+++ dashboard/layout.py
@@ -108,7 +108,7 @@ layout = html.Div([  # This is the main layout of the app
             ],
         ),
 
-        dmc.Button("Clean Data", id="clean-data-button"),
+        # dmc.Button("Clean Data", id="clean-data-button"),
         # dmc.Button("Cancel", id="cancel-button", disabled=True),
         # dmc.Checkbox(id="auto-clean-checkbox", label="Auto Clean First?", checked=True),
         # dmc.Text(id="log-textbox"),
